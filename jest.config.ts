6f8a15cf967d7c71c9116f5e8b7903b08f9c91d0
Michael Khirallah <mike.mkrallaproductions@gmail.com>
2022-10-01 13:37:53 -0400

    fix incorrect config

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from '@jest/types';
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
-  modulePathIgnorePatterns: ['src/gitTestData'],
+  modulePathIgnorePatterns: ['src/gitTestData', 'dist'],
   transform: {
     '^.+\\.(ts|tsx|js)$': 'ts-jest',
   },
